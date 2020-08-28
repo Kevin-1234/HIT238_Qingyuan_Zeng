745c9f2f93b978d2340b62e4a749de3beacececa
KevinLynn <60030989+Kevin-1234@users.noreply.github.com>
2020-08-28 11:31:35 +0930

    Update main.js

--- script/main.js
+++ script/main.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
-    navigator.serviceWorker.register('./sw.js').then(function(registration) {
+    navigator.serviceWorker.register('/HIT238_Qingyuan_Zeng/sw.js').then(function(registration) {
       // Registration was successful
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
     }, function(err) {
